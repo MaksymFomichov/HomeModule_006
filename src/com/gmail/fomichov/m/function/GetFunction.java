1a2009ca142a75a055267484e62c8b801b94e500
Maksym <m.fomichov@gmail.com>
2017-07-27 02:44:38 +0300

    reload

--- src/com/gmail/fomichov/m/function/GetFunction.java
+++ src/com/gmail/fomichov/m/function/GetFunction.java
@@ -1,4 +1,37 @@
 package com.gmail.fomichov.m.function;
 
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+import java.util.Arrays;
+
 public class GetFunction {
+
+
+    public Line[] getAxis() {
+        Line[] lineAxis = new Line[2];
+        lineAxis[0] = new Line(Main.WIDTH_SCREEN / 2, 0, Main.WIDTH_SCREEN / 2, Main.HEIGHT_SCREEN);
+        lineAxis[1] = new Line(0, Main.HEIGHT_SCREEN / 2, Main.WIDTH_SCREEN, Main.HEIGHT_SCREEN / 2);
+        return lineAxis;
+    }
+
+    public Circle[] getX2() {
+        int value = -400;
+        Circle[] circles = new Circle[value * -2 + 1];
+        for (int i = 0; i < circles.length; i++) {
+            circles[i] = new Circle(getRealX(value), getRealY(Math.pow(value, 2)), 2);
+            //circles[i] = new Circle(getRealX(value), getRealY(Math.pow(value, 3)), 2);
+            // circles[i] = new Circle(getRealX(value), getRealY(Math.cos(value)), 2);
+            value++;
+        }
+        return circles;
+    }
+
+    private double getRealX(double x) {
+        return Main.WIDTH_SCREEN / 2 - x;
+    }
+
+    private double getRealY(double y) {
+        return Main.HEIGHT_SCREEN / 2 - y;
+    }
 }
--- src/com/gmail/fomichov/m/function/Main.java
+++ src/com/gmail/fomichov/m/function/Main.java
@@ -1,4 +1,49 @@
 package com.gmail.fomichov.m.function;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static final int WIDTH_SCREEN = 800;
+    public static final int HEIGHT_SCREEN = 800;
+    private GetFunction getFunction;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setWidth(WIDTH_SCREEN);
+        primaryStage.setHeight(HEIGHT_SCREEN);
+        getFunction = new GetFunction();
+
+        Button button = new Button();
+        button.setTranslateX(10);
+        button.setTranslateY(10);
+        button.setText("Получить график");
+
+        Pane root = new Pane();
+        root.getChildren().addAll(button);
+        root.getChildren().addAll(getFunction.getAxis());
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("График функции");
+        primaryStage.show();
+
+        button.setOnMouseClicked(event -> {
+            // чистим экран (нужно для следующих нажатий на кропку "нарисовать")
+            root.getChildren().clear();
+            // передаем данные для отрисовки
+            root.getChildren().addAll(getFunction.getX2());
+            // сохраняем текущие данные для повторного вывода на экран и выводим их
+            root.getChildren().addAll(button);
+            root.getChildren().addAll(getFunction.getAxis());
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
